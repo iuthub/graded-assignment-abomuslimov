5e73e48fed86f4ccd80a1e17d726122ae7f629b5
abomuslimov <45311775+abomuslimov@users.noreply.github.com>
2020-04-18 00:20:16 +0500

    finished all tasks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,153 +8,93 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/8500f75e5b.js" crossorigin="anonymous"></script>
 
         <!-- Styles -->
         <style>
-            /* Include the padding and border in an element's total width and height */
-            * {
-              box-sizing: border-box;
-              font-family: Nunito, san-serif;
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
             }
 
-            /* Remove margins and padding from the list */
-            ul {
-              margin: 0;
-              padding: 0;
+            .full-height {
+                height: 100vh;
             }
 
-            /* Style the list items */
-            ul li {
-              position: relative;
-              padding: 12px 8px 12px 40px;
-              background: #eee;
-              font-size: 18px;
-              transition: 0.2s;
-              display: flex;
-              flex-flow: row nowrap;
-              align-items: center;
-              justify-content: flex-start;
-
-
-              /* make the list items unselectable */
-              -webkit-user-select: none;
-              -moz-user-select: none;
-              -ms-user-select: none;
-              user-select: none;
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
             }
 
-            /* Set all odd list items to a different color (zebra-stripes) */
-            ul li:nth-child(odd) {
-              background: #f9f9f9;
+            .position-ref {
+                position: relative;
             }
 
-            /* Darker background-color on hover */
-            ul li:hover {
-              background: #ddd;
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
             }
 
-            ul li .task {
-                flex-grow: 1;
+            .content {
+                text-align: center;
             }
 
-            ul li .action {
-                margin: 5px 15px;
-                vertical-align: middle;
+            .title {
+                font-size: 84px;
             }
 
-            ul li a {
-                color: grey;
-            }
-         
-            /* Style the header */
-            .header {
-              background-color: #f44336;
-              padding: 30px 40px;
-              color: white;
-              text-align: center;
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
             }
 
-            /* Clear floats after the header */
-            .header:after {
-              content: "";
-              display: table;
-              clear: both;
-            }
-
-            /* Style the input */
-            input {
-              margin: 0;
-              border: none;
-              border-radius: 0;
-              width: 75%;
-              padding: 10px;
-              float: left;
-              font-size: 16px;
-            }
-
-            /* Style the "Add" button */
-            .addBtn {
-              padding: 10px;
-              width: 25%;
-              background: #d9d9d9;
-              color: #555;
-              float: left;
-              text-align: center;
-              font-size: 16px;
-              cursor: pointer;
-              transition: 0.3s;
-              border-radius: 0;
-              border: none;
-            }
-
-            .addBtn:hover {
-              background-color: #bbb;
+            .m-b-md {
+                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <form action="">
-            <div id="myDIV" class="header">
-              <h2>My To Do List</h2>
-              <input type="text" name="newTask" placeholder="Title...">
-              <button type="submit" class="addBtn">Add</button>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
             </div>
-        </form>
-        <ul id="myUL">
-          <li>
-            <div class="task">
-                Hit the gym
-            </div>
-            <div class="action">
-                <a href=""><i class="fa fa-edit"></i></a>
-            </div>
-            <div class="action">
-                <a href=""><i class="fa fa-trash-alt"></i></a> 
-            </div>
-          </li>
-          <li>
-            <div class="task">
-               Make some food
-            </div>
-            <div class="action">
-                <a href=""><i class="fa fa-edit"></i></a>
-            </div>
-            <div class="action">
-                <a href=""><i class="fa fa-trash-alt"></i></a> 
-            </div>
-          </li>
-          <li>
-            <div class="task">
-               Finish quiz
-            </div>
-            <div class="action">
-                <a href=""><i class="fa fa-edit"></i></a>
-            </div>
-            <div class="action">
-                <a href=""><i class="fa fa-trash-alt"></i></a> 
-            </div>
-          </li>
-        </ul>
+        </div>
     </body>
 </html>
